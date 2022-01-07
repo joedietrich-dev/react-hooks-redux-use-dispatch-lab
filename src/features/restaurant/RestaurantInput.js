7d37d6426811dff51113cc7a9c6f6e5cf4d1d2af
joedietrich-dev <joe@joedietrich.net>
2022-01-06 22:16:06 -0500

    Done.

--- src/features/restaurant/RestaurantInput.js
+++ src/features/restaurant/RestaurantInput.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { addRestaurant } from "./restaurantsSlice";
 
 function RestaurantInput() {
   const [name, setName] = useState("");
   const [location, setLocation] = useState("");
+
+  const dispatch = useDispatch();
 
   function handleNameChange(event) {
     setName(event.target.value);
@@ -15,26 +18,16 @@ function RestaurantInput() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    // add missing code
+    dispatch(addRestaurant({ name, location }));
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <p>
-        <input
-          type="text"
-          onChange={handleNameChange}
-          id="name"
-          placeholder="restaurant name"
-        />
+        <input type="text" onChange={handleNameChange} id="name" placeholder="restaurant name" />
       </p>
       <p>
-        <input
-          type="text"
-          onChange={handleLocationChange}
-          id="location"
-          placeholder="location"
-        />
+        <input type="text" onChange={handleLocationChange} id="location" placeholder="location" />
       </p>
       <button type="submit">Submit</button>
     </form>
